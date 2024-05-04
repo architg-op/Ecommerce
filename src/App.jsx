ce228b213e86002107446979840c3ed3a9320fd0
architg-op <archit.g1998@gmail.com>
2024-05-04 18:18:53 +0530

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import { useState } from 'react';
+import Router from './router/Router';
+
+function App() {
+  const [allRoutes, setAllRoutes] = useState([]);
+
+  return <Router allRoutes={allRoutes} />;
 }
+
+export default App;
