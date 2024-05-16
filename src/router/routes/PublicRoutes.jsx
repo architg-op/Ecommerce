d546e0536977c1da4605323a8273f5a6fce05282
architg-op <archit.g1998@gmail.com>
2024-05-16 16:18:16 +0530

    Landing page added

--- src/router/routes/PublicRoutes.jsx
+++ src/router/routes/PublicRoutes.jsx
@@ -1,9 +1,13 @@
 import { lazy } from 'react';
+const LandingPage = lazy(() => import('../../views/pages/LandingPage'));
 const Login = lazy(() => import('../../views/auth/Login'));
 const Register = lazy(() => import('../../views/auth/Register'));
 const AdminLogin = lazy(() => import('../../views/auth/AdminLogin'));
-
 const PublicRoutes = [
+  {
+    path: '/Ecommerce',
+    element: <LandingPage />,
+  },
   {
     path: '/Ecommerce/login',
     element: <Login />,
---
+++ src/views/pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+function LandingPage() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: '#cdcae9',
+      }}
+    >
+      Landing Page will be built shortly, currently login, register and
+      admin-login routes are available to explore.
+    </div>
+  );
+}
+
+export default LandingPage;
